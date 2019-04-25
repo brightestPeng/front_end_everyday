087bb363171f4c09aa6a477f2ae404f7d2c7ea46
幻影 <997450807@qq.com>
2019-04-25 18:27:16 +0800

    提交代码

---
+++ 2019-04-25/src/layout/tablelayout/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./index.less";
+
+const index = ()=>{
+
+	return(
+		<div className="tableLayout" >
+			<div className="box" ></div>
+		</div>
+	)
+}
+
+export default index;
