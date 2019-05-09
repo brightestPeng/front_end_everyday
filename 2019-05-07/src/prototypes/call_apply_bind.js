bf937ca86492bb0cf4a93693008ff7b9557a4b07
peng <997450807@qq.com>
2019-05-09 23:25:57 +0800

    提交代码

--- 2019-05-07/src/prototypes/call_apply_bind.js
+++ 2019-05-07/src/prototypes/call_apply_bind.js
@@ -1,21 +1,75 @@
-Function.prototype.myCall = function(){
-	let _self = arguments[0];
-	let _this = this;
-	(()=>{
-		this = _self;
-		_this();
-	})()
+Function.prototype.myCall = function(context){
+	context = context || window;
+	context.fn = this;
+	let args =[];
+
+	for(let i=1;i<arguments.length;i++){
+		args.push("arguments["+i+"]");
+	}
+
+	let result = eval("context.fn("+args+")");
+
+	delete context.fn;
+	return result;
+}
+
+Function.prototype.myApply = function(context,arr){
+	context = context || window;
+	context.fn = this;
+	let args = [],
+		result;
+
+	if(arr instanceof Array){
+		for(var i=0;i<arr.length;i++){
+			args.push('arr['+i+']');
+		}
+		result = eval("context.fn("+args+")");
+	}else{
+		result = context.fn();
+	}
+
+	delete context.fn;
+	return result;
 }
 
 
-let obj = {
-	name:"peng"
-}
+// Function.prototype.bind = function(context){
+// 	let _self = this;
+// 	return function(){
+// 		_self.apply(context,arguments);
+// 	}
+// }
 
-let name = "wen";
 
-function getName(){
-	console.log(this.name);
-}
+// let obj = {
+// 	name:"peng"
+// }
 
-getName.myCall(obj,"a","b","c");
+// let name = "wen";
+
+// function getName(a,b,c){
+// 	console.log(a);
+// 	console.log(b);
+// 	console.log(c);
+
+// 	console.log(this.name);
+// }
+
+// getName.myApply(obj,["a","b","c"]);
+
+// console.log(a);
+
+// // var a = "bb";
+// function a(){
+
+// }
+
+var foo = 1;
+
+var fooReference = {
+    base: EnvironmentRecord,
+    name: 'foo',
+    strict: false
+};
+
+GetValue(fooReference) 
